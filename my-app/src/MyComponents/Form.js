72526dfcdfcd6e3664181842b6ddc2663418b774
PankajSinghSheoran <pankajsheoran728@gmail.com>
2022-10-08 20:37:33 +0530

    Nothing new just testing

---
+++ my-app/src/MyComponents/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <div>
+      <h4>This form can be used to provide feedback</h4>
+    </div>
+  );
+};
+
+export default Form;
