5641a415c3b266ad33384bf7d4a2f36585f1c0bb
PankajSinghSheoran <pankajsheoran728@gmail.com>
2022-10-10 21:15:33 +0530

    Adding new things

--- my-app/src/MyComponents/ToDos.js
+++ my-app/src/MyComponents/ToDos.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { TodoItem } from "./TodoItem";
+import { ToDoItem } from "./ToDoItem";
 
 export const ToDos = (props) => {
   return (
     <div className="container">
       <h4 className="text-center">This is my ToDo list</h4>
-      <TodoItem todo={props.todos[0]} />
+      <ToDoItem todo={props.todos[0]} />
     </div>
   );
 };
