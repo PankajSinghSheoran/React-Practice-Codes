5bb7e54c821d5ea2642d0a21fb7d573488bfa494
Pankaj Singh <pankajsheoran728@gmail.com>
2022-10-11 20:06:54 +0530

    Nothing new

--- my-app/src/MyComponents/ToDos.js
+++ my-app/src/MyComponents/ToDos.js
@@ -4,7 +4,7 @@ import { ToDoItem } from "./ToDoItem";
 export const ToDos = (props) => {
   return (
     <div className="container">
-      <h4 className="text-center">This is my ToDo list</h4>
+      <h1 className="text-center">This is my ToDo list</h1>
       <ToDoItem todo={props.todos[0]} />
     </div>
   );
